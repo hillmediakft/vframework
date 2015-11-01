f9e03d01a0e5f5ace7a948ae7eb1c2e8d6c1d3c8
varnagy-attila <vucuka@gmail.com>
2015-11-01 15:27:20 +0100

    hooks config

---
+++ system/config/hooks.php
@@ -0,0 +1,24 @@
+<?php
+$config['hook'] = array();
+/*
+$config['hook'] = array(
+  'admin_pre_system' => array(
+    'class' => 'Hello',
+    'function' => 'print_pre_system',
+    'filename' => 'hello.php',
+    'filepath' => 'system/libs'
+  ),
+  'admin_post_controller_constructor' => array(
+    'class' => 'Hello',
+    'function' => 'print_post_controller_constructor',
+    'filename' => 'hello.php',
+    'filepath' => 'system/libs'
+  ),
+  'site_pre_system' => array(
+    'class' => 'Router',
+    'function' => 'route',
+    'filename' => 'router.php',
+    'filepath' => 'hooks'
+  )
+); */
+?>
