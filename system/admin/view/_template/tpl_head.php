887554935a8526e47283883f669fbaa1c2830f97
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2015-11-05 19:01:26 +0100

    testimonials bekepcsolva

--- system/admin/view/_template/tpl_head.php
+++ system/admin/view/_template/tpl_head.php
@@ -211,10 +211,10 @@
                                     <a href="admin/slider">
                                         Slider beállítások</a>
                                 </li>
-                     <!--           <li class="<?php echo ($this->request->get_controller() == 'testimonials') ? 'active' : ''; ?>">
+                                <li class="<?php echo ($this->request->get_controller() == 'testimonials') ? 'active' : ''; ?>">
                                     <a href="admin/testimonials">
                                         Rólunk mondták</a>
-                                </li> -->
+                                </li>
                             </ul>
 
                         </li>
