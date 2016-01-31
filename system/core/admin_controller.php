e5ba150a6137e798882fd307fb752f145ecd3639
Zsolti <varnagyzsolt1975@gmail.com>
2016-01-31 11:54:00 +0100

    admin_contoller - update

--- system/core/admin_controller.php
+++ system/core/admin_controller.php
@@ -1,9 +1,14 @@
 <?php 
 class Admin_controller extends Controller {
+
+    // felhasználói jogosultság ellenőrzéséhez
+    protected $user;
 	
 	public function __construct()
 	{
 		parent::__construct();
+        //require_once "system/libs/logged_in_user.php";
+        $this->user = new Logged_in_user();
 	}
         
     /**
@@ -13,10 +18,11 @@ class Admin_controller extends Controller {
      * 	@param	string	$target_url jogosultság hiányában ide irányítjuk a felhasználót
      * 	@return	bool	ha van jogosultsága a felhasználónak, akkor true értéket ad vissza
      */
-    public function check_access($perm, $target_url = 'home') {
+    public function check_access($perm, $target_url = 'home')
+    {
         if (!$this->user->hasAccess($perm)) {
             Message::set('error', 'Nincs jogosultsága a művelet végrehajtásához!');
-            $target_url = str_replace(BASE_URL . "admin/","",$target_url);
+            $target_url = str_replace(BASE_URL . "admin/", "", $target_url);
             Util::redirect($target_url);
         }
         return true;
