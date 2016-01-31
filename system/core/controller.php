b566c74ebdb4d9d34232ae808f570dcaf191ee23
varnagy-attila <vucuka@gmail.com>
2016-01-31 11:47:33 +0100

    Autoversion a css és js fájlokhoz

--- system/core/controller.php
+++ system/core/controller.php
@@ -1,72 +1,92 @@
 <?php
+
 class Controller {
 
     /**
-	 *	A HTML oldal megjelenítést végző objektum rendelődik hozzá ehhez a tulajdonsághoz
-	 *	A kontruktorban jön létre
-	 */
-	protected $view;
-	
-	public $registry;
-	
-	function __construct()
-	{
-		Session::init();
-        
+     * 	A HTML oldal megjelenítést végző objektum rendelődik hozzá ehhez a tulajdonsághoz
+     * 	A kontruktorban jön létre
+     */
+    protected $view;
+    public $registry;
+
+    function __construct() {
+        Session::init();
+
         $this->registry = Registry::get_instance();
-		$this->request = $this->registry->request;
-		// létrehozzuk a view objektumot és hozzárendeljük a $view tulajdonsághoz
-		$this->view = new View();
-	}
-	
-	/*
-	 *	A model file betöltése és példányosítása a paramétertől függően.
-	 *
-	 *	@param	string	(a model file neve kiterjesztés nélkül)
-	 */
-	public function loadModel($model)
-	{
-		// model file elérési útja
-		$file = 'system/' . $this->request->get_uri('area') . '/model/' . $model . '.php';
+        $this->request = $this->registry->request;
+        // létrehozzuk a view objektumot és hozzárendeljük a $view tulajdonsághoz
+        $this->view = new View();
+    }
 
-		// megnézzük, hogy megnyitható-e a file, ha nem kivételt dobunk.
-		try {
-			if (!file_exists($file)) {
-				throw new Exception('A ' . $file . ' file nem nyithato meg!');
-			}
-		} catch (Exception $e)  {
-			die('<br /><strong>Hiba:</strong> ' . $e->getMessage() . '<br />');
-		}
+    /*
+     * 	A model file betöltése és példányosítása a paramétertől függően.
+     *
+     * 	@param	string	(a model file neve kiterjesztés nélkül)
+     */
 
-		// model file behívása
-		require_once($file);
+    public function loadModel($model) {
+        // model file elérési útja
+        $file = 'system/' . $this->request->get_uri('area') . '/model/' . $model . '.php';
 
-		// a file betöltődése után létrejöhet az objektum, mert a $model nevű file tartalamazza a $model nevű osztályt
-		// létrejön egy új tulajdonság a controller objektumban, a neve pedig a $model változóban tárolt string lesz
-		// ehhez a tulajdonsághoz rendelődik az új model objektum, aminek a neve szintén a $model változóban tárolt string
-		// pl.: $this->model_users = new model_users();
-		$this->$model = new $model();
-	}	
-	
-	/**
-	 *	Kezdetleges link összeállító metódus
-	 *
-	 *	@param	string	$type	a link/script típusa: css vagy js
-	 *	@param	string	$path	a link útvonala, ami állandókban van (pl.: ADMIN_CSS, SITE_ASSETS)
-	 *	@param	string	$link	a link további útvonala (pl.: plugins/data-tables/DT_bootstrap.css)
-	 */
-	public function make_link($type, $path, $link)
-	{
-		$string = '';
-		switch($type){
-			case 'css':
-				$string .= '<link rel="stylesheet" href="' . $path . $link . '" type="text/css" />' . "\r\n";
-				break;	
-			case 'js':
-				$string .= '<script type="text/javascript" src="' . $path . $link . '"></script>' . "\r\n";
-				break;	
-		}
-		return $string;
-	}
-}	
+        // megnézzük, hogy megnyitható-e a file, ha nem kivételt dobunk.
+        try {
+            if (!file_exists($file)) {
+                throw new Exception('A ' . $file . ' file nem nyithato meg!');
+            }
+        } catch (Exception $e) {
+            die('<br /><strong>Hiba:</strong> ' . $e->getMessage() . '<br />');
+        }
+
+        // model file behívása
+        require_once($file);
+
+        // a file betöltődése után létrejöhet az objektum, mert a $model nevű file tartalamazza a $model nevű osztályt
+        // létrejön egy új tulajdonság a controller objektumban, a neve pedig a $model változóban tárolt string lesz
+        // ehhez a tulajdonsághoz rendelődik az új model objektum, aminek a neve szintén a $model változóban tárolt string
+        // pl.: $this->model_users = new model_users();
+        $this->$model = new $model();
+    }
+
+    /**
+     * 	Kezdetleges link összeállító metódus
+     *
+     * 	@param	string	$type	a link/script típusa: css vagy js
+     * 	@param	string	$path	a link útvonala, ami állandókban van (pl.: ADMIN_CSS, SITE_ASSETS)
+     * 	@param	string	$link	a link további útvonala (pl.: plugins/data-tables/DT_bootstrap.css)
+     */
+    public function make_link($type, $path, $link) {
+        $string = '';
+        switch ($type) {
+            case 'css':
+                $string .= '<link rel="stylesheet" href="' . $path . $link . '" type="text/css" />' . "\r\n";
+                break;
+            case 'js':
+                $string .= '<script type="text/javascript" src="' . $path . $link . '"></script>' . "\r\n";
+                break;
+        }
+        return $string;
+    }
+
+    /**
+     * 	Verziőval ellátott link összeállító metódus
+     *
+     * 	@param	string	$type	a link/script típusa: css vagy js
+     * 	@param	string	$path	a link útvonala, ami állandókban van (pl.: ADMIN_CSS, SITE_ASSETS)
+     * 	@param	string	$link	a link további útvonala (pl.: plugins/data-tables/DT_bootstrap.css)
+     */
+    public function make_versioned_link($type, $path, $link) {
+        $string = '';
+        switch ($type) {
+            case 'css':
+                $string .= '<link rel="stylesheet" href="' . Util::auto_version($path . $link) . '" type="text/css" />' . "\r\n";
+                break;
+            case 'js':
+                $string .= '<script type="text/javascript" src="' . Util::auto_version($path . $link) . '"></script>' . "\r\n";
+                break;
+        }
+        return $string;
+    }
+
+}
+
 ?>
--- system/libs/util_class.php
+++ system/libs/util_class.php
@@ -1,104 +1,100 @@
-<?php 
-class Util
-{
+<?php
+
+class Util {
+
     /**
      * Redirects to another page.
      *
-	 * @param string $location The path to redirect to
+     * @param string $location The path to redirect to
      * @param int $status Status code to use
      * @return bool False if $location is not set
      */
     public static function redirect($location, $status = 302) {
-		$registry = Registry::get_instance();
-		$request = $registry->request;
-		
-		if ($location == '') {
-			header("Location: " . $request->get_uri('site_url'), true, $status);
-			exit;
-		}
-		
-		header("Location: " . $request->get_uri('site_url') . $location, true, $status);
-		exit;
+        $registry = Registry::get_instance();
+        $request = $registry->request;
+
+        if ($location == '') {
+            header("Location: " . $request->get_uri('site_url'), true, $status);
+            exit;
+        }
+
+        header("Location: " . $request->get_uri('site_url') . $location, true, $status);
+        exit;
     }
-	
-	/**
-	 * Ellenőrzi, hogy a Ajax hívás történt-e
-	 *
-	 * @return bool true|false
-	 */
-	public static function is_ajax()
-	{
-		if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-		   return true;
-		}
-		return false;
-	}
-	
-	/**
-	 *	File törlése
-	 *
-	 *	@param	$filename	a törlendő file elérésiútja mappa/filename.ext
-	 *	@return	true|false
-	 */
-	public static function del_file($filename)
-	{
-		if (is_file($filename)) {
-			//ha a file megnyitható és írható
-			if(is_writable($filename)){
-				unlink($filename);
-				return true;
-			}
-		}
-		else {
-			return false;
-		}
-	}
-	
-	/**
-	 * Egy kép elérési útvonalából generál egy elérési útvonalat a bélyegképéhez
-	 * Minta: path/to/file/filename.jpg -> path/to/file/filename_thumb.jpg
-	 * 
-	 * @param	string	$path (a file elérési útja)
-	 * @param	bool	$thumb (hozzárak az elérési út végéhez egy thumb mappát)
-	 * @return	string	a bélyegkép elérési útvonala
-	 */
-	public static function thumb_path($path, $thumb = false)
-	{
-		$path_parts = pathinfo($path);
-		$dirname = $path_parts['dirname'];
-		$filename = $path_parts['filename'];
-		$extension = $path_parts['extension'];
-		
-		if(!$thumb){
-			if(($dirname == '.') || ($dirname == '\\')) {
-				$path_with_thumb = $filename . '_thumb.' . $extension;
-			} else {
-				$path_with_thumb = $dirname . '/' . $filename . '_thumb.' . $extension;
-			}
-		} else {
-			if(($dirname == '.') || ($dirname == '\\')) {
-				$path_with_thumb = 'thumb/' . $filename . '_thumb.' . $extension;
-			} else {
-				$path_with_thumb = $dirname . '/thumb/' . $filename . '_thumb.' . $extension;
-			}
-		
-		}
-		return $path_with_thumb;
-	}
-	
-	/**
-	 *	Visszaadja a jelenlegi url-t a paraméterben megadott nyelvi kóddal módosítva
-	 *
-	 *	@param	String	$lang_code	(nyelvi kód)
-	 *	@return	String
-	 */
+
+    /**
+     * Ellenőrzi, hogy a Ajax hívás történt-e
+     *
+     * @return bool true|false
+     */
+    public static function is_ajax() {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 	File törlése
+     *
+     * 	@param	$filename	a törlendő file elérésiútja mappa/filename.ext
+     * 	@return	true|false
+     */
+    public static function del_file($filename) {
+        if (is_file($filename)) {
+            //ha a file megnyitható és írható
+            if (is_writable($filename)) {
+                unlink($filename);
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Egy kép elérési útvonalából generál egy elérési útvonalat a bélyegképéhez
+     * Minta: path/to/file/filename.jpg -> path/to/file/filename_thumb.jpg
+     * 
+     * @param	string	$path (a file elérési útja)
+     * @param	bool	$thumb (hozzárak az elérési út végéhez egy thumb mappát)
+     * @return	string	a bélyegkép elérési útvonala
+     */
+    public static function thumb_path($path, $thumb = false) {
+        $path_parts = pathinfo($path);
+        $dirname = $path_parts['dirname'];
+        $filename = $path_parts['filename'];
+        $extension = $path_parts['extension'];
+
+        if (!$thumb) {
+            if (($dirname == '.') || ($dirname == '\\')) {
+                $path_with_thumb = $filename . '_thumb.' . $extension;
+            } else {
+                $path_with_thumb = $dirname . '/' . $filename . '_thumb.' . $extension;
+            }
+        } else {
+            if (($dirname == '.') || ($dirname == '\\')) {
+                $path_with_thumb = 'thumb/' . $filename . '_thumb.' . $extension;
+            } else {
+                $path_with_thumb = $dirname . '/thumb/' . $filename . '_thumb.' . $extension;
+            }
+        }
+        return $path_with_thumb;
+    }
+
+    /**
+     * 	Visszaadja a jelenlegi url-t a paraméterben megadott nyelvi kóddal módosítva
+     *
+     * 	@param	String	$lang_code	(nyelvi kód)
+     * 	@return	String
+     */
     public static function url_with_language($lang_code = 'hu') {
- 		$registry = Registry::get_instance();
-		$lang = ($lang_code == 'hu') ? '' : $lang_code . '/';
-		$area = ($registry->area == 'site') ? '' : $registry->area . '/';
+        $registry = Registry::get_instance();
+        $lang = ($lang_code == 'hu') ? '' : $lang_code . '/';
+        $area = ($registry->area == 'site') ? '' : $registry->area . '/';
         return BASE_URL . $area . $lang . $registry->uri;
     }
-	
+
     /**
      * Spamektől védett e-mail linket generál Javascripttel
      *
@@ -171,38 +167,56 @@ class Util
         $x = array_reverse($x);
         ob_start();
         ?><script type="text/javascript">
-        //<![CDATA[
-                    var l = new Array();
+            //<![CDATA[
+            var l = new Array();
         <?php
         $i = 0;
         foreach ($x as $val) {
             ?>l[<?php echo $i++; ?>] = '<?php echo $val; ?>';<?php } ?>
 
-                    for (var i = l.length - 1; i >= 0; i = i - 1) {
-                        if (l[i].substring(0, 1) == '|')
-                            document.write("&#" + unescape(l[i].substring(1)) + ";");
-                        else
-                            document.write(unescape(l[i]));
-                    }
-        //]]>
+                for (var i = l.length - 1; i >= 0; i = i - 1) {
+                    if (l[i].substring(0, 1) == '|')
+                        document.write("&#" + unescape(l[i].substring(1)) + ";");
+                    else
+                        document.write(unescape(l[i]));
+                }
+                //]]>
         </script><?php
         $buffer = ob_get_contents();
         ob_end_clean();
         return $buffer;
     }
-	
-/**
-*	Ékezetes karaktereket és a szóközt cseréli le ékezet nélkülire és alulvonásra
-*	minden karaktert kisbetűre cserél
-*/	
-	public static function string_to_slug($string)
-	{
-	   $accent = array ("&", " ", "-", "á", "é", "í" , "ó", "ö", "ő", "ú", "ü", "ű", "Á", "É", "Í" , "Ó", "Ö", "Ő", "Ú", "Ü", "Ű");
-	   $no_accent = array ('and','_', '_', 'a', 'e', 'i' , 'o', 'o', 'o', 'u', 'u', 'u', 'A', 'E', 'I' , 'O', 'O', 'O', 'U', 'U', 'U');
-	   $string = str_replace($accent, $no_accent, $string);	   
-	   $string = strtolower($string);
-	   return $string;
-	}	
-	
-}	
+
+    /**
+     * 	Ékezetes karaktereket és a szóközt cseréli le ékezet nélkülire és alulvonásra
+     * 	minden karaktert kisbetűre cserél
+     */
+    public static function string_to_slug($string) {
+        $accent = array("&", " ", "-", "á", "é", "í", "ó", "ö", "ő", "ú", "ü", "ű", "Á", "É", "Í", "Ó", "Ö", "Ő", "Ú", "Ü", "Ű");
+        $no_accent = array('and', '_', '_', 'a', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'u', 'A', 'E', 'I', 'O', 'O', 'O', 'U', 'U', 'U');
+        $string = str_replace($accent, $no_accent, $string);
+        $string = strtolower($string);
+        return $string;
+    }
+
+    /**
+     * A fájl nevéhez hozzáilleszt egy query stringet (pl: style.css?v=2314564321
+     * A szám a fájl utolsó módosításának timestamp-je
+     *  
+     * @param   string  $uri  a file elérési útvonala pl.: valami,hu/public/site_assets/css/style.css
+     * @return  string  a fájl verzióval ellátott elérési útvonala
+     */
+    public static function auto_version($uri) {
+        if (substr($uri, 0, 1) == "/") {
+            // relatív URI
+            $fname = $_SERVER["DOCUMENT_ROOT"] . $uri;
+        } else {
+            // abszolút URI
+            $fname = $uri;
+        }
+        $ftime = filemtime($fname);
+        return $uri . '?v=' . $ftime;
+    }
+
+}
 ?>
