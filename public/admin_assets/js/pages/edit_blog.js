3640fb2f87a006bcd72b2b56d3db3ca155127d57
Zsolti <varnagyzsolt1975@gmail.com>
2016-02-02 18:16:49 +0100

    admin edit_blog.js file hozzaadva

---
+++ public/admin_assets/js/pages/edit_blog.js
@@ -0,0 +1,27 @@
+var editBlog = function () {
+
+	var hideAlert = function () {
+		$('div.alert').delay( 2500 ).slideUp( 750 );						 		
+	}
+
+    var ckeditorInit = function () {
+        CKEDITOR.replace( 'blog_body', {customConfig: 'config_custom3.js'});
+    }
+
+    return {
+        //main function to initiate the module
+        init: function () {
+			hideAlert();
+			ckeditorInit();
+        }
+    };
+
+}();
+
+jQuery(document).ready(function() {    
+	Metronic.init(); // init metronic core componets
+	Layout.init(); // init layout
+	QuickSidebar.init(); // init quick sidebar
+	Demo.init(); // init demo features	
+	editBlog.init();
+});
