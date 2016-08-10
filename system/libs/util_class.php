09441b5f16bc808c830dc6deeb2341b8d567fbbb
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-08-10 13:05:39 +0200

    util class - thumb_path() metodus fejlesztve, url_with_language() metodus eltavolitva

--- system/libs/util_class.php
+++ system/libs/util_class.php
@@ -23,24 +23,13 @@ class Util {
     }
 
     /**
-     * Ellenőrzi, hogy a Ajax hívás történt-e
-     *
-     * @return bool true|false
-     */
-    public static function is_ajax() {
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-            return true;
-        }
-        return false;
-    }
-
-    /**
      * 	File törlése
      *
      * 	@param	$filename	a törlendő file elérésiútja mappa/filename.ext
      * 	@return	true|false
      */
-    public static function del_file($filename) {
+    public static function del_file($filename)
+    {
         if (is_file($filename)) {
             //ha a file megnyitható és írható
             if (is_writable($filename)) {
@@ -56,43 +45,32 @@ class Util {
      * Egy kép elérési útvonalából generál egy elérési útvonalat a bélyegképéhez
      * Minta: path/to/file/filename.jpg -> path/to/file/filename_thumb.jpg
      * 
-     * @param	string	$path (a file elérési útja)
-     * @param	bool	$thumb (hozzárak az elérési út végéhez egy thumb mappát)
-     * @return	string	a bélyegkép elérési útvonala
+     * @param   string  $path (a file elérési útja)
+     * @param   bool    $dir (hozzárak az elérési út végéhez egy mappát, amit a $suffix paraméterben kap meg a metódus)
+     * @param   string  $suffix (fájl nevének utótagja)
+     * @return  string  a bélyegkép elérési útvonala
      */
-    public static function thumb_path($path, $thumb = false) {
+    public static function thumb_path($path, $dir = false, $suffix = 'thumb')
+    {
         $path_parts = pathinfo($path);
         $dirname = $path_parts['dirname'];
         $filename = $path_parts['filename'];
         $extension = $path_parts['extension'];
 
-        if (!$thumb) {
+        if (!$dir) {
             if (($dirname == '.') || ($dirname == '\\')) {
-                $path_with_thumb = $filename . '_thumb.' . $extension;
+                $new_path = $filename . '_' . $suffix . '.' . $extension;
             } else {
-                $path_with_thumb = $dirname . '/' . $filename . '_thumb.' . $extension;
+                $new_path = $dirname . '/' . $filename . '_' . $suffix . '.' . $extension;
             }
         } else {
             if (($dirname == '.') || ($dirname == '\\')) {
-                $path_with_thumb = 'thumb/' . $filename . '_thumb.' . $extension;
+                $new_path = $suffix . '/' . $filename . '_' . $suffix . '.' . $extension;
             } else {
-                $path_with_thumb = $dirname . '/thumb/' . $filename . '_thumb.' . $extension;
+                $new_path = $dirname . '/' . $suffix . '/' . $filename . '_' . $suffix . '.' . $extension;
             }
         }
-        return $path_with_thumb;
-    }
-
-    /**
-     * 	Visszaadja a jelenlegi url-t a paraméterben megadott nyelvi kóddal módosítva
-     *
-     * 	@param	String	$lang_code	(nyelvi kód)
-     * 	@return	String
-     */
-    public static function url_with_language($lang_code = 'hu') {
-        $registry = Registry::get_instance();
-        $lang = ($lang_code == 'hu') ? '' : $lang_code . '/';
-        $area = ($registry->area == 'site') ? '' : $registry->area . '/';
-        return BASE_URL . $area . $lang . $registry->uri;
+        return $new_path;
     }
 
     /**
