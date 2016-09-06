b068a9681c8feedcd68cd8acb625211a7629eea9
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-09-06 13:54:56 +0200

    autoloader update (egyelore kikommentelve)

--- system/core/autoloader.php
+++ system/core/autoloader.php
@@ -11,6 +11,7 @@ class Autoloader {
 	public function __construct()
 	{
     	spl_autoload_register(array($this, 'autoload'));
+    	//spl_autoload_register(array($this, 'autoload_phpMailer'));
 	}
 
     public function autoload($class_name)
@@ -31,5 +32,15 @@ class Autoloader {
 			return; 
 		}
     }
+/*
+	public function autoload_phpMailer($class_name)
+	{
+		$file = LIBS . '/PHPMailer/class.' . strtolower($class_name) . '.php';
+		if (file_exists($file)){
+			require $file;
+			return; 
+		}			
+	}
+*/
 }
 ?>
