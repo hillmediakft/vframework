37c50c65133481a4612df9515518b84870907569
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-09-06 13:51:03 +0200

    session class update

--- system/libs/session_class.php
+++ system/libs/session_class.php
@@ -35,17 +35,73 @@ class Session
         $_SESSION[$key] = $value;
     }
 
-	/**
- 	 * gets/returns the value of a specific key of the session
-	 * @param mixed $key Usually a string, right ?
-	 * @return mixed
-	 */
+
+			/**
+		 	 * gets/returns the value of a specific key of the session
+			 * @param mixed $key Usually a string, right ?
+			 * @return mixed
+			 */
+			/*
+		    public static function get($key)
+		    {
+		        if (isset($_SESSION[$key])) {
+		            return $_SESSION[$key];
+		        }
+		    }
+		    */
+
+    /**
+     * Visszaadja a $_SESSION tömb megadott kulcsának értékét
+     * A többdimenziós tömb esetén a kulcs megadása: key.subkey 
+     * 
+     * @param string $key 		pl: 'username' vagy 'userinput.name'
+     */
     public static function get($key)
     {
-        if (isset($_SESSION[$key])) {
-            return $_SESSION[$key];
-        }
+    	if (strpos($key, '.') === false) {
+	    	if (isset($_SESSION[$key])) {
+	            return $_SESSION[$key];
+	        }
+    	} else {
+			return self::_get_array_value($_SESSION, $key);
+    	}
     }
+
+    /**
+     * Elem törlése a $_SESSION tömbből
+     *
+     * @param string $key  
+     */
+    public static function delete($key)
+    {
+	    if(isset($_SESSION[$key])){
+            unset($_SESSION[$key]);    
+        } 
+    }
+
+	/**
+	 *	Visszadja egy tömb megadott kulcsának értékét
+	 *
+	 *	Példa a kulcsok megadására: (a többdimenziós tömb elemeit a . karakterrel elválasztva kell megadni, mintha egy útvonal lenne)
+	 *		_get_array_value($array, 'userinput.firstname');
+	 *
+	 *	@param   array   $array    Ebből a tömbből adjuk vissza az adatot
+	 *	@param   mixed   $key      A kulcs, amit keresünk
+	 *	@return  mixed
+	 */
+	private static function _get_array_value($array, $key)
+	{
+		foreach (explode('.', $key) as $key_part)
+		{
+			if (!array_key_exists($key_part, $array))
+			{
+				return false;
+			}
+			$array = $array[$key_part];
+		}
+
+		return $array;
+	}
 
 	/**
 	 * deletes the session (= logs the user out)
