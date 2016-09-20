709211f7afe7d157b1688b13690abf9210e72f18
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-09-20 18:09:49 +0200

    teszt email sablon letrehozva

---
+++ system/site/view/email/tpl_tesztmail.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>{title}</title>
+
+</head>
+<body>
+
+    <h1>szioka</h1>
+    <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur.</p>
+
+    <p style="color:green;"><strong>Név: </strong>{from_name}</p>
+    <p><strong>Email: </strong>{from_email}</p>
+
+    <p style="color:red;">
+        <strong>Üzenet:</strong><br>
+        {message}
+    </p>
+
+</body>
+</html>
