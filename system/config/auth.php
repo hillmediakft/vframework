da777a1f8e57bef9ce8e0c913721d000a90b3d53
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-09-20 18:22:10 +0200

    Uj Auth class, es config file

---
+++ system/config/auth.php
@@ -0,0 +1,45 @@
+<?php 
+$config['auth'] = array(
+
+	// DB table name for the user table
+	'table_name' => 'users',
+	//Choose which columns are selected
+	'table_columns' => array('*'),
+	//'guest_login' => true,
+
+	/**
+	 * Remember-me functionality
+	 */
+	'remember_me' => array(
+		// Whether or not remember me functionality is enabled
+		'enabled' => false,
+		//Name of the cookie used to record this functionality
+		'cookie_name' => 'tesztcookie',
+		// Remember me expiration (default: 31 days)
+		'expiration' => 86400 * 31
+	),
+
+	/**
+	 * A user tábla username oszlop neve
+	 */
+	'username_colname' => 'user_name',
+	/**
+	 * A user tábla username oszlop neve
+	 */
+	'email_colname' => 'user_email',
+	/**
+	 * A user tábla password oszlop neve
+	 */
+	'password_colname' => 'user_password_hash',
+	/**
+	 * Átirányítás helye: pl.: login (ilyenkor a site_url/login oldalra fog irányítani)
+	 * Ez lesz az alapbeállítás, ha a handleExpire(), vagy handleLogin() metódust paraméter nélkül hívjuk meg
+	 */
+	'target_url' => 'login',
+
+	/**
+	 * Munkamenet lejárati ideje
+	 */
+	'session_expire_time' => 3600,
+);
+?>
