d4b83a7c6bdd5b756763366358a12f153a05d889
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-10-27 14:13:34 +0200

    photo gallery thumb hight meret hozzaadva

--- system/config/common_config.php
+++ system/config/common_config.php
@@ -53,13 +53,16 @@ $config['slider'] = array(
     'width' => 1170,
     'height' => 420,
     'thumb_width' => 200,
+    'thumb_height' => 72,
     'upload_path' => UPLOADS . 'slider_photo/'
 );
 
 $config['photogallery'] = array(
     'width' => 800,
+    'height' => 600,
     'thumb_width' => 320,
-    'upload_path' => UPLOADS . 'photo_gallery'
+    'thumb_height' => 240,
+    'upload_path' => UPLOADS . 'photo_gallery/'
 );
 
 $config['hash_cost_factor'] = 10;
