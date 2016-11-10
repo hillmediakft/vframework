000dfee9702f8b2ac84301dbcdcf776f25a9a21a
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-11-10 15:34:51 +0100

    mod

--- system/site/controller/ajax_request.php
+++ system/site/controller/ajax_request.php
@@ -1,52 +1,21 @@
 <?php
 namespace System\Site\Controller;
+
 use System\Core\Site_controller;
-Use System\Libs\Util;
 
 class Ajax_request extends Site_controller {
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
-        $this->loadModel('ajax_request_model');
+        //$this->loadModel('ajax_request_model');
     }
 
-    public function index() {
-        Util::redirect('error');
+    public function index()
+    {
+        $this->response->redirect('error');
     }
-   
-    
-    /**
-     *  AJAX Email küldés
-     */
-    public function ajax_send_email() {
-        if (Util::is_ajax()) {
-
-            $from_email = ;
-            $from_name = ;
-            $message = ;
-            $to_email = ;
-            $to_name = $to_email;
-            $subject = '';
-            
-            $result = $this->ajax_request_model->send_email($from_email, $from_name, $subject, $message, $to_email, $to_name);
-
-            if ($result) {
-                $message = array(
-                  'status' => 'success',
-                  'message' => 'Pozitív üzenet!'
-                );
-                echo json_encode($message);
-            } else {
-                $message = array(
-                  'status' => 'error',
-                  'message' => 'Negatív üzenet!'
-                );
-                echo json_encode($message);
-            }
-        } else {
-            Util::redirect('error');
-        }
-    }
+  
 
 }
 ?>
