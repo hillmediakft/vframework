ee16386b07441e41c4528281cc96036641cd327e
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-11-10 15:35:01 +0100

    mod

--- system/core/admin_controller.php
+++ system/core/admin_controller.php
@@ -1,7 +1,7 @@
 <?php
 namespace System\Core;
 
-use System\Libs\Auth;
+//use System\Libs\Auth;
 
 class Admin_controller extends Controller {
 
@@ -10,11 +10,13 @@ class Admin_controller extends Controller {
         parent::__construct();
         
         // Authentikáció minden admin oldalra, kivéve a login
+        /*
         if($this->request->get_controller() != 'login'){
            	if (!Auth::check()) {
                 $this->response->redirect('admin/login');	
            	} 
         }
+        */
      
     }
 }
