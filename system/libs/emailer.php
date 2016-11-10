dec4316243abd3f3dbf0205842985f1c665c16a4
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-11-10 15:36:10 +0100

    az autoload include eltavolitva

--- system/libs/emailer.php
+++ system/libs/emailer.php
@@ -114,7 +114,7 @@ class Emailer {
      */
     public function send()
     {
-        include(LIBS . '/PHPMailer/PHPMailerAutoload.php');
+        //include(LIBS . '/PHPMailer/PHPMailerAutoload.php');
         $mail = new PHPMailer;
 
         if ($this->use_smtp) {
