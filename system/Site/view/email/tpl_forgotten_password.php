c6fd19f9ca2cee873421f73a65fc7c110d3ccecc
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-21 11:53:12 +0100

    email template-ek hozzaadva

---
+++ system/Site/view/email/tpl_forgotten_password.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>{title}</title>
+
+</head>
+<body>
+
+    <h3>Kedves {user_name}!</h3>
+    
+    <p>Az ön új jelszava {base_url} című oldalunkhoz.</p>
+    
+    <p><strong>{new_password}</strong></p>
+    
+    <p>Üdvözlettel:<br>**********</p>
+
+</body>
+</html>
---
+++ system/Site/view/email/tpl_user_register_verification.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>{title}</title>
+
+</head>
+<body>
+
+    <h3>Kedves {user_name}!</h3>
+    
+    <p>A {user_email} e-mail címmel regisztráltál a {base_url} című oldalunkra.</p>
+    
+    <p> Regisztrációd megtörtént, de jelenleg passzív.</p>
+    
+    <a href="{base_url}felhasznalo/ellenorzes/{user_id}/{user_activation_hash}">Klikkel erre a linkre a regisztraciód aktiválásához!</a>
+    
+    <p>Az aktiválást követően a {base_url} oldalára jutsz, ahol bejelentkezhetsz a felhasználó neveddel és jelszavaddal.</p>
+    
+    <p>Üdvözlettel:<br>**********</p>
+
+</body>
+</html>
