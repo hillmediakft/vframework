894558904d1add8a4943d891e9e7a80c7cc315a1
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-10-13 10:18:56 +0200

    _checkLangVersion() metodus hozzaadva

--- system/Core/AdminModel.php
+++ system/Core/AdminModel.php
@@ -15,5 +15,24 @@ class AdminModel extends Model {
 		$this->langs = Config::get('allowed_languages');
 	}
 
+	/**
+     * Megadott nyelvi kódú elem létezését vizsgálja egy táblában
+     *
+     * @param string $table - tábla neve
+     * @param string $column - oszlop neve (blog_id, category_id stb.)
+     * @param integer $id
+     * @param string $langcode
+     * @return bool
+     */
+    public function _checkLangVersion($table, $column, $id, $langcode)
+    {
+        $this->query->set_table($table);
+    	$this->query->set_columns('COUNT(id) AS counter');
+    	$this->query->set_where($column, '=', $id);
+    	$this->query->set_where('language_code', '=', $langcode);
+    	$result = $this->query->select();
+    	return ($result[0]['counter'] == 1) ? true : false;
+    }
+
 }
 ?>
